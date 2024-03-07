7919a4a299fd7dbd1fdf5a5f54b3fb4f6439a5b5
mkoumoudjian <61242150+mkoumoudjian@users.noreply.github.com>
2024-03-06 21:21:41 -0500

    Update streamlit_app.py
    remove comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,15 @@ ingredients_string = ''
 NAME_ON_ORDER = st.text_input("add order name");
 my_insert_stmt = ''
 
-#if ingridents_lists:
- #   for fruit_chosen in ingridents_lists:
-  #      ingredients_string += fruit_chosen + ' '
-   # st.write(ingredients_string)
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
-     #       values ('""" + ingredients_string + """', '""" + NAME_ON_ORDER + """')"""
+if ingridents_lists:
+    for fruit_chosen in ingridents_lists:
+        ingredients_string += fruit_chosen + ' '
+    st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
+            values ('""" + ingredients_string + """', '""" + NAME_ON_ORDER + """')"""
     
-    #time_to_insert = st.button('submit_order')
-    #if ingredients_string and time_to_insert:
-     #   session.sql(my_insert_stmt).collect()
-      #  st.success('Your Smoothie is ordered,' + NAME_ON_ORDER + '!', icon="✅")
-       # st.write(my_insert_stmt)
+    time_to_insert = st.button('submit_order')
+    if ingredients_string and time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered,' + NAME_ON_ORDER + '!', icon="✅")
+        st.write(my_insert_stmt)
