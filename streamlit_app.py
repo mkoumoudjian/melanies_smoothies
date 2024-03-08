d32eee0b8053cd0e68365e0b07ce05460b3187e6
mkoumoudjian <61242150+mkoumoudjian@users.noreply.github.com>
2024-03-08 10:04:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,4 +30,4 @@ if ingridents_lists:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
